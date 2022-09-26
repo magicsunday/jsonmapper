920f37e6f9d2199c4e6f9f3ac9663c3c2fce483c
Rico Sonntag <mail@ricosonntag.de>
2022-09-26 07:53:42 +0200

    PHP8.2 compatibility

--- test/Classes/Person.php
+++ test/Classes/Person.php
@@ -18,8 +18,9 @@ namespace MagicSunday\Test\Classes;
  * @license https://opensource.org/licenses/MIT
  * @link    https://github.com/magicsunday/jsonmapper/
  *
- * @property int $oscars
+ * @property int $oscars Dynamic created property
  */
+#[\AllowDynamicProperties]
 class Person
 {
     /**
