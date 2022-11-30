9ccd1ff41fb3c5681158245e34f6a8b7c3d0884a
Rico Sonntag <mail@ricosonntag.de>
2022-11-30 09:18:07 +0100

    Add camelCase converter test

---
+++ test/Converter/CamelCasePropertyNameConverterTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the package magicsunday/jsonmapper.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MagicSunday\Test\Converter;
+
+use MagicSunday\JsonMapper\Converter\CamelCasePropertyNameConverter;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CamelCasePropertyNameConverterTest
+ *
+ * @author  Rico Sonntag <mail@ricosonntag.de>
+ * @license https://opensource.org/licenses/MIT
+ * @link    https://github.com/magicsunday/jsonmapper/
+ */
+class CamelCasePropertyNameConverterTest extends TestCase
+{
+    /**
+     * Tests mapping json properties to camel case.
+     *
+     * @test
+     */
+    public function checkCamelCasePropertyNameConverter(): void
+    {
+        $converter = new CamelCasePropertyNameConverter();
+
+        self::assertSame('camelCaseProperty', $converter->convert('camelCaseProperty'));
+        self::assertSame('camelCaseProperty', $converter->convert('camel_case_property'));
+        self::assertSame('camelCaseProperty', $converter->convert('camel-case-property'));
+        self::assertSame('camelCaseProperty', $converter->convert('camel case property'));
+    }
+}
